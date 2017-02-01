e10f3b30a5c77a8601e285c101ee3de8b25ebde0
LOICSLAPTOP\loics <loicszymanski@gmail.com>
2017-02-01 15:46:02 +0100

    add functions

--- compteEstBon.hs
+++ compteEstBon.hs
@@ -14,6 +14,12 @@ valid Sub x y = x > y -- do not allow for negative integers
 valid Mul _ _ = True -- always valid
 valid Div x y = mod x y == 0 -- integer division only
 
+calculate :: Op -> Int -> Int -> Int
+calculate Add x y = x + y
+calculate Sub x y = x - y
+calculate Mul x y = x * y
+calculate Div x y = x `div` y
+
 data Expr = Val Int | App Op Expr Expr
 instance Show Expr where
   show (Val x) = show x
@@ -22,3 +28,27 @@ instance Show Expr where
 values :: Expr -> [Int]
 values (Val n) = [n]
 values (App _ l r) = values l ++ values r
+
+eval :: Expr -> [Int]
+eval (Val n) = [n | n > 0]
+eval (App o l r) = [calculate o x y | x <- eval l, y <- eval r, valid o x y]
+
+subs :: [a] -> [[a]]
+subs [] = [[]]
+subs (x:xs) = yss ++ map (x:) yss
+  where yss = subs xs
+
+
+interleave :: a -> [a] -> [[a]]
+interleave x [] = [[]]
+interleave x (y:ys) = (x:y:ys) : map (y:) (interleave x ys)
+
+perms :: [a] -> [[a]]
+perms [] = [[]]
+perms (x:xs) = concat (map (interleave x) (perms xs))
+
+ssens :: [a] -> [[a]]
+ssens = concat . map perms . subs
+
+solution :: Expr -> [Int] -> Int -> Maybe Expr
+solution ns n = [ e | ns' <- subbags ns, e <- exprs ns'), eval e == [n]] 
